285737871ee4632828dd83a2c42045d13b3a13ee
ryosebach <ryo.freezia@gmail.com>
2017-11-11 09:21:04 +0900

    fix init script

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-##############
-#  git init  #
-##############
+test -r ~/.dotfiles/etc/install && . ~/.dotfiles/etc/install && os_detect
 
+cd ~/.dotfiles/etc/init/"$PLATFORM"/
+
+for script in *; do
+	e_newline && e_header "Run File $script"
+	cat $script | bash
+done
