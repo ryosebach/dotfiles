287ae0e3454653280a33c1dacc339200a7dee2d3
ryosebach <ryo.freezia@gmail.com>
2017-11-11 09:19:24 +0900

    add setup trackpad option

--- etc/init/osx/09_mac_setup.sh
+++ etc/init/osx/09_mac_setup.sh
@@ -1,2 +1,4 @@
 echo "cp ~/.dotfiles/etc/lib/osx/plist/com.apple.dock.plist ~/Library/Preferences/"
 cp ~/.dotfiles/etc/lib/osx/plist/com.apple.dock.plist ~/Library/Preferences/
+echo "cp ~/.dotfiles/etc/lib/osx/plist/com.apple.driver.AppleBluetoothMultitouch.trackpad.plist ~/Library/Preferences/"
+cp ~/.dotfiles/etc/lib/osx/plist/com.apple.driver.AppleBluetoothMultitouch.trackpad.plist ~/Library/Preferences/
