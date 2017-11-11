b045751b1508fe260d576a33f16e42753ce413f7
ryosebach <ryo.freezia@gmail.com>
2017-11-11 10:53:31 +0900

    fix cask app install script

--- etc/init/osx/04_cask_app_install.sh
+++ etc/init/osx/04_cask_app_install.sh
@@ -1,15 +1,53 @@
-brew cask install unity
+ConfirmApp=(
+"kap"
+"insomnia"
+"hyperswitch"
+"spectacle"
+"unity"
+"visual-studio"
+)
+yes_or_no(){
+	while true; do
+		read answer
+		case $answer in
+		yes)
+			return 0
+			;;
+		y) 
+			return 0
+			;;
+		no)
+			return 1
+			;;
+		N)
+			return 1
+			;;
+		n)
+			return 1
+			;;
+		*)
+			echo -e "please say yes or no"
+			;;
+		esac
+	done
+}
+brew tap caskroom/versions
 brew cask install alfred
 brew cask install clipy
 brew cask install google-chrome
 brew cask install google-japanese-ime
-brew cask install franz
-brew tap caskroom/versions
 brew cask install google-chrome-canary
-brew cask instal the-unarchiver
+brew cask install franz
+brew cask install the-unarchiver
 brew cask install vlc
 brew cask install adobe-creative-cloud
-brew cask install kap
-brew cask install spectacle
-brew cask install insomnia
-brew cask install hyperswitch
+
+for app in ${ConfirmApp[@]}; do
+	echo "Installing $app... OK? (y, N)"
+	if yes_or_no; then
+		echo "Install $app"
+		brew cask install $app
+	else
+		echo "Skip installing $app"
+	fi
+done
