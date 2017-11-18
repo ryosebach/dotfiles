d990213b57701b83dbdf92cc7864ee8f891992c6
ryosebach <ryo.freezia@gmail.com>
2017-11-18 13:18:24 +0900

    add solarized download

--- etc/init/osx/05_other_app_install.sh
+++ etc/init/osx/05_other_app_install.sh
@@ -1,7 +1,21 @@
+#!/bin/bash
+
 #Install nodebrew
 test -r ~/.dotfiles/etc/install && . ~/.dotfiles/etc/install
 
 e_newline && e_header "Install Nodebrew"
 curl -L git.io/nodebrew | perl - setup
 
+e_newline && e_header "Terminal Solarized Download"
 
+tarball="https://github.com/tomislav/osx-terminal.app-colors-solarized/archive/master.tar.gz"
+
+cd ~/Downloads
+mkdir solarized
+if is_exists "curl"; then
+  curl -L "$tarball"
+elif is_exists "wget"; then
+  wget -O - "$tarball"
+fi | tar xvz -C solarized --strip-components 1
+
+open solarized/
