b2ffd5df01a451627a2aae99e002eadca98ce81e
ryosebach <ryo.freezia@gmail.com>
2018-04-11 19:20:28 +0900

    chore : remove unusefull space

--- etc/init/linux/02_apt_install.sh
+++ etc/init/linux/02_apt_install.sh
@@ -1,9 +1,9 @@
-AptApp=(                   
+AptApp=(
 	libncurses5-dev
 	build-essential
 	golang-1.10-go
-)                           
+) 
 
 for app in ${AptApp[@]}; do
-	sudo apt install $app         
-done                        
+	sudo apt install $app
+done 
