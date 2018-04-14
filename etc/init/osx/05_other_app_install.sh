6c7a83ff3e03d88e105a08a14bc40b8be0721c63
ryosebach <ryo.freezia@gmail.com>
2018-04-15 01:46:47 +0900

    chore : other app download to appropriate folder

--- etc/init/osx/05_other_app_install.sh
+++ etc/init/osx/05_other_app_install.sh
@@ -6,22 +6,22 @@ test -r ~/.dotfiles/etc/install && . ~/.dotfiles/etc/install
 e_newline && e_header "Install Nodebrew"
 curl -L git.io/nodebrew | perl - setup
 
-e_newline && e_header "Terminal Solarized Download"
+e_newline && e_header "Terminal Solarized & Azusa-Colors Download"
 
 TarBalls=(
 "https://github.com/sanographix/azusa-colors/archive/master.tar.gz"
 "https://github.com/tomislav/osx-terminal.app-colors-solarized/archive/master.tar.gz"
 )
 
-count=0
 cd ~/Downloads
 for tarball in ${TarBalls[@]}; do
-	mkdir "tarball${count}"
+	tmpName=${tarball#*//*/*/}
+	DirName=${tmpName%%/*}
+	mkdir ${DirName}
 	if is_exists "curl"; then
 	  curl -L "$tarball"
 	elif is_exists "wget"; then
 	  wget -O - "$tarball"
-	fi | tar xvz -C "tarball${count}" --strip-components 1
-	open "tarball${count}"
-	count=`echo "$count+1" | bc`
+	fi | tar xvz -C ${DirName} --strip-components 1
+	open ${DirName}
 done
