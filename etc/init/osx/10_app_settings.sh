1ffc21284aef33ed9579943be1fc007894edc2ef
ryosebach <ryo.freezia@gmail.com>
2018-05-01 10:48:37 +0900

    feat : add osx init script for app-settings

---
+++ etc/init/osx/10_app_settings.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+test -r ~/.dotfiles/etc/install && . ~/.dotfiles/etc/install && os_detect
+
+e_newline && e_arrow "some settings installing"
+
+e_newline && e_header "mp3tag setting installing"
+mp3tagDir=`\ls -1 ~/Library/Application\ Support/ | grep mp3tag`
+if [ ! -z "$mp3tagDir" ] ; then
+	echo "mp3tag setting"
+	echo "[Software\\\\Wine\\\\Fonts\\\\Replacements]" >> ~/Library/Application\ Support/$mp3tagDir/user.reg
+	echo "\"MS UI Gothic\"=\"Hiragino Maru Gothic ProN W4\"" >> ~/Library/Application\ Support/$mp3tagDir/user.reg
+fi
