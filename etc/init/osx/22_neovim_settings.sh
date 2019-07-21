eea239dc8751dec246bbad04a1674335114b54d9
ryosebach <ryo.freezia@gmail.com>
2019-07-21 14:10:00 +0900

    :sparkles: Install vim-plug for neovim.

--- etc/init/osx/22_neovim_settings.sh
+++ etc/init/osx/22_neovim_settings.sh
@@ -4,3 +4,7 @@ test -r ~/.dotfiles/etc/install && . ~/.dotfiles/etc/install && os_detect
 
 pip install neovim
 pip3 install neovim
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
